b1c01d2bb97a39a3f5e33eefe8260c9bfc971a1f
Ante Zorica <azorica@extensionengine.com>
2025-04-25 10:48:14 +0200

    typescript playground 4

--- plant_a_tree_vite_vue3/models/user_test.ts
+++ plant_a_tree_vite_vue3/models/user_test.ts
@@ -19,3 +19,38 @@ function greet(person: User, date: Date) {
 }
  
 greet(user, new Date());
+
+let obj: any = { x: 0 };
+// None of the following lines of code will throw compiler errors.
+// Using `any` disables all further type checking, and it is assumed
+// you know the environment better than TypeScript.
+obj.foo();
+obj();
+obj.bar = 100;
+obj = "hello";
+const n: number = obj;
+
+let myName: string = "Alice";
+
+// This is a function that takes a string and returns a number
+function returnNumberForString(str: string): number {
+  return parseInt(str);
+}
+
+returnNumberForString(myName);
+
+const makePizza = (ingredients: string) => {
+  console.log(`Making pizza with ${ingredients}`);
+};
+
+// The parameter's type annotation is an object type
+function printCoord(pt: { x: number, y?: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3 });
+
+let x = Math.random() < 0.5 ? 10 : "hello world!";
+
+console.log(x); // x is of type 'string | number'
+//x = true; // Error: Type 'true' is not assignable to type 'string | number'
